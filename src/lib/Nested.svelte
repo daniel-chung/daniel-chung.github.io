33f9e9acc6f1f94fa49ed59c46870a93f0da93d6
Daniel Chung <chung.dan.s@gmail.com>
2025-08-25 16:45:24 -0400

    Initial commit

---
+++ src/lib/Nested.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	let name = 'Daniel Chung';
+</script>
+
+<h1 class="text-2xl font-mono">{name}</h1>
+<p class="text-base font-mono">Site under construction</p>
+
+
+<style lang="postcss">
+	@reference "tailwindcss";
+	:global(html) {
+		background-color: theme(--color-gray-100);
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import Nested from '$lib/Nested.svelte';
+</script>
+
+<Nested />
